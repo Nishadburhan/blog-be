69fbadb07e6fb4b863c2ea7bd24f331e45f34533
nishadburhan <nishadburhanofficial@gmail.com>
2024-05-16 17:54:49 +0530

    User Flow User Signup User Signin Auth Middleware

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 3000;
+
+const userRoute = require("./src/controllers/userController");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/api", userRoute);
+
 app.get("/", (req, res) => {
     res.send("welcome to the blog");
 });
+
+try {
+    mongoose.connect(process.env.DB_URL);
+} catch (error) {
+    console.error(error);
+}
 
 app.listen(port, (req, res) => {
     console.log(`Server listening on: http://localhost:${port}`);
---
+++ src/models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isActive: {
+        type: Boolean,
+        default: false
+    },
+    deleted_at: { 
+        type: Date,
+        default: null
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
